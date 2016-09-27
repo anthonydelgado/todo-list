ac02048a07aab2157d9bd4622c793e52072364b9
Anthony Delgado <me@anthonydelgado.me>
2016-09-27 18:55:08 -0400

    MVP

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-
+$('#add').on('click', function () {
+    var newTodo = $('#newtask').val();
+    var listItem = $('<li class="collection-item">');
+    listItem.text(newTodo);
+    $('#tasks').append(listItem);
+    $("#newtask").val('');
+});
